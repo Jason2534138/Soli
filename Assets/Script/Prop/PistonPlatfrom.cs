d0dc82cf359ef422845bf33ec5a49c97f9f2ed30
Jason2534138 <blin8863@gmail.com>
2025-09-19 11:16:53 +0800

    20250919平台碰撞

--- Assets/Script/Prop/PistonPlatfrom.cs
+++ Assets/Script/Prop/PistonPlatfrom.cs
@@ -5,27 +5,54 @@ using UnityEngine;
 public class PistonPlatfrom : MonoBehaviour
 {
     [SerializeField] private Transform[] _path;
-    private int current;
+    private int current = 0;
     private Vector2 _refSpeed = Vector2.zero;
     private float _isColliding;
-    
+    private Rigidbody2D _rb;
+    [SerializeField] private float _speed;
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private Transform[] _detect;
+    private int _detectCurrent = 0;
+    [SerializeField] public Vector2 boxSize;
+    //private bool _isBlocked;
+
     private void Start()
     {
+        _rb = GetComponent<Rigidbody2D>();
         this.transform.position = _path[0].position;
     }
-    private void LateUpdate()
+    private void Update()
     {
-        if (!(_isColliding > 0)) this.transform.position = Vector2.SmoothDamp((Vector2)this.transform.position, (Vector2)_path[current].position, ref _refSpeed, 3f);
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Prop")) _isColliding -= 1;
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
+        if (Input.GetKeyDown(KeyCode.T)) ChangePath();
+        //foreach (Transform t in _detect)
+        //{
+        //    Collider2D hit = Physics2D.OverlapCircle(t.position, 0.1f, _layerMask);
+        //    if (hit == null) _isBlocked = false;
+        //    else _isBlocked = true;
+        //}
+        if (Vector2.Distance(this.transform.position, _path[current].position) > 0.1f && !Physics2D.OverlapBox(_detect[_detectCurrent].position, boxSize, 0, _layerMask)) 
+        {
+            Vector3 dir;
+            dir = (_path[current].position - transform.position).normalized;
+            _rb.MovePosition(_rb.position + (Vector2)(dir * _speed * Time.deltaTime));
+
+        }
+        else _rb.velocity = Vector2.zero;
         
-        if (collision.gameObject.CompareTag("Prop")) _isColliding += 1;
+    }
+    
+    private void ChangePath()
+    {
         current += 1;
         if (current >= _path.Length) current = 0;
+        _detectCurrent = current + 1;
+        if (_detectCurrent >= _detect.Length) _detectCurrent = 0;
+    }
+    private void OnDrawGizmos()
+    {
+        foreach (Transform t in _detect)
+        {
+            Gizmos.DrawWireCube(t.position, boxSize);
+        }
     }
 }
--- Assets/Script/Prop/WallPlatfrom.cs
+++ Assets/Script/Prop/WallPlatfrom.cs
@@ -1,47 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class WallPlatfrom : MonoBehaviour
 {
     [SerializeField] private Transform[] _path;
-    private int current;
+    [SerializeField] private Transform[] _detect;
+    private int _detectCurrent = 0;
+    private int current = 0;
     private Vector2 _refSpeed = Vector2.zero;
     private float _isColliding;
+    [SerializeField]private LayerMask _layerMask;
     Rigidbody2D _rb;
     [SerializeField] private float _speed;
+    //rivate bool _isBlocked = false;
 
     private void Start()
     {
-        this.transform.position = _path[0].position;
+        this.transform.position = _path[current].position;
         _rb = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.P)) ChangePath();
-        if(Vector2.Distance(this.transform.position, _path[current].position) > 0.1f && !(_isColliding > 0))
+        //foreach (Transform t in _detect)
+        //{
+        //    Collider2D hit = Physics2D.OverlapCircle(t.position, 0.01f, _layerMask);
+        //    if (hit == null) _isBlocked = false;
+        //    else _isBlocked = true;
+        //}
+        if (Vector2.Distance(this.transform.position, _path[current].position) > 0.1f && !Physics2D.OverlapCircle(_detect[_detectCurrent].position, 0.1f, _layerMask))
         {
-            Debug.Log("moving");
-            Vector2 vel;
             Vector3 dir;
-            vel = _rb.velocity;
             dir = (_path[current].position - transform.position).normalized;
-            _rb.velocity = _speed * dir;
-            Debug.Log(_rb.velocity);
-        }else _rb.velocity = Vector2.zero;
-        //if (!(_isColliding > 0)) this.transform.position = Vector2.SmoothDamp((Vector2)this.transform.position, (Vector2)_path[current].position, ref _refSpeed, 0.1f);
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Prop")) _isColliding += 1;
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Prop")) _isColliding -= 1;
+            _rb.MovePosition(_rb.position + (Vector2)(dir * _speed * Time.deltaTime));
+            
+
+        }
+        else _rb.velocity = Vector2.zero;
+        
     }
     private void ChangePath()
     {
         current += 1;
         if (current >= _path.Length) current = 0;
+        _detectCurrent = current + 1;
+        if (_detectCurrent >= _detect.Length) _detectCurrent = 0;
     }
 }
