4b1d1c9091343bf0dca8c3f3bbce2a3967a3b1dc
Beepine <raychen930610@gmail.com>
2025-11-04 21:10:01 +0800

    2025/11/4
    設計機關、新的想法嘗試

--- Assets/Script/Player/Detector.cs
+++ Assets/Script/Player/Detector.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -51,5 +52,10 @@ public class Detector : MonoBehaviour
     {
         Gizmos.DrawWireCube(transform.position-transform.up * castDistance, boxSize);
         Gizmos.DrawWireCube(transform.position+transform.right * wallCastDistance, wallBoxSize);
+    }
+
+    internal bool IsTouchingWall()
+    {
+        throw new NotImplementedException();
     }
 }
