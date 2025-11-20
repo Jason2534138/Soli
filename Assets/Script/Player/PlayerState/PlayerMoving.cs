7759d6393048a0d527eb4ed4bd6764c9f3e4b457
Jason2534138 <blin8863@gmail.com>
2025-11-21 00:10:16 +0800

    20251120VER.2
    ver2

--- Assets/Script/Player/PlayerState/PlayerMoving.cs
+++ Assets/Script/Player/PlayerState/PlayerMoving.cs
@@ -8,6 +8,7 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PlayerMoving : PlayerGrounded
 {
+    
     public PlayerMoving(PlayerMovementSM stateMachine) : base("PlayerMoving", stateMachine) 
     {
         _sm = (PlayerMovementSM)stateMachine;
@@ -21,6 +22,7 @@ public class PlayerMoving : PlayerGrounded
     {
         base.LogicUpdate();
         _sm.animator.Play("Player_run 0");
+        _sm.transform.parent = null;
         _horizontalInput = Input.GetAxis("Horizontal");
         if (Mathf.Abs(_horizontalInput) < Mathf.Epsilon)
         {
